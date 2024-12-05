d82d55e4f6cbe767e12f23495cce2b9078696359
Sumit Kilaniya <sumitkilaniya8950@gmail.com>
2024-12-05 12:09:09 +0530

    Add files via upload

---
+++ web d training/PROJECT 1/nasa/indexs/index.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7, "hello", true];
+for (let i = 0; i <arr.length; i++) {
+    console.log (arr [i] );
+}
+arr.push("sandeep")
+console.log("..............................");
+
+for (let i=0; i<arr.length;i++) {
+    console.log (arr[i]);
+}
---
+++ web d training/day6/day6.js
@@ -0,0 +1,64 @@
+// // // console.log("-----welcome in js------")
+// // // const a = 8 ;
+// // // let b = 6 ;
+// // // var r = a + b;
+// // // console.log(a+b);
+// // // console.log("HELLO JAVASCRIPT");
+// // // console.log("---------variables in js-------- ");
+// // // var var1=5;
+// // // var var2="varianle-2-in-string-format";
+// // // var var3=7653498729;
+// // // var var4=[2,3,4,5,6,"hello"];
+
+// // // console.log(var1,var2,var3);
+
+
+
+
+
+
+
+// // let a = 5;
+// // let b = 6;
+// // let c = 7;
+// // let d = 8;
+// // let e = 99;
+// // console.log(a+b+c+d+e);
+// // console.log("average = ",(a+b+c+d+e)/5);
+// // let largest_number =Math.max(a,b,c,d,e);
+// // console.log(largest_number); 
+    
+// // let num1=23;
+// // let num2=34;
+// // let num3=56;
+// // let largest;
+
+// // if (num1 < num3 && num2<num3){
+// //     largest = num3;
+// // } else if(num1<num2&&num3<num2){
+// //     largest = num2;
+// // }else{
+// //     largest = num1;
+// // }
+// // console.log("the largest number is :" +largest);
+// let i;
+// for(let i=0; i<100; i++){
+//     if(i%3==0 && i%5==0 && i%9==0){
+ 
+// const newValue= "  "+ i + " = " + 10 * i;
+// console.log(newValue);
+// }
+// }
+// for(let i=0; i<100; i=i+3){
+//     console.log(i)
+// }
+// let i=0;
+// let n=10;
+// while(i<n){
+//     console.log(i)
+//     i++
+// }
+const num1=prompt("enter a number : ");
+for(let i=num1  ; i>0; i--){
+    console.log(i)
+}
